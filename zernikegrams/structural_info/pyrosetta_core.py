2e8941969b16a889eabad33157d92e6cd85537ed
Gian Marco Visani <gvisan01@n3124.hyak.local>
2024-06-05 15:30:46 -0700

    before switching

--- zernikegrams/structural_info/pyrosetta_core.py
+++ zernikegrams/structural_info/pyrosetta_core.py
@@ -6,6 +6,7 @@ from typing import *
 
 import pyrosetta
 init_flags = '-ignore_unrecognized_res 1 -include_current -ex1 -ex2 -mute all -include_sugars -ignore_zero_occupancy false -obey_ENDMDL 1'
+
 pyrosetta.init(init_flags, silent=True)
 
 from pyrosetta.rosetta.core.pose import Pose
