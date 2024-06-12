1d6b9acacd4cb1c802d1da6fac93e752506a1757
William Galvin <williamgalvin246@gmail.com>
2024-06-12 10:19:23 -0700

    run pre-link with prefix + correct install location

--- devtools/conda-build/pre-link.sh
+++ devtools/conda-build/pre-link.sh
@@ -5,13 +5,16 @@ set -e
 echo "PREFIX is $PREFIX" >> $PREFIX/.messages.txt
 
 # install reduce -- only available from source
-echo "installing Reduce" >> $PREFIX/.messages.txt
-export CMAKE_INSTALL_PREFIX=$HOME/local
+echo "installing Reduce" >> $PREFIX/.messages.txt 
 git clone https://github.com/rlabduke/reduce.git
 mv reduce $PREFIX
 
 mkdir -p $PREFIX/build/reduce
 cd $PREFIX/build/reduce
-cmake $PREFIX/reduce >> $PREFIX/.messages.txt
+cmake $PREFIX/reduce -DCMAKE_INSTALL_PREFIX=$HOME/local >> $PREFIX/.messages.txt
 make >> $PREFIX/.messages.txt
 make install >> $PREFIX/.messages.txt
+
+rm -rf $PREFIX/reduce
+rm -rf $PREFIX/build/reduce
+rm $PREFIX/reduce_wwPDB_het_dict.txt
