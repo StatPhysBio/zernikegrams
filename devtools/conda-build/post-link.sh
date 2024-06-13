d27919936478841a44eba32da5f62c2d67cf6d55
William Galvin <williamgalvin246@gmail.com>
2024-06-13 10:53:32 -0700

    fix post-link

--- devtools/conda-build/post-link.sh
+++ devtools/conda-build/post-link.sh
@@ -5,19 +5,16 @@ set -e
 mkdir -p $PREFIX
 touch $PREFIX/.messages.txt
 
-echo "PREFIX is $PREFIX" >> $PREFIX/.messages.txt
+echo "PREFIX is $PREFIX"
 
 # install reduce -- only available from source
-echo "installing Reduce" >> $PREFIX/.messages.txt 
+echo "installing Reduce"  
+[ -d "reduce" ] && rm -rf reduce
 git clone https://github.com/rlabduke/reduce.git
-mv reduce $PREFIX
+[ -d "$PREFIX/reduce" ] && rm -rf reduce || mv reduce $PREFIX
 
 mkdir -p $PREFIX/build/reduce
 cd $PREFIX/build/reduce
-cmake $PREFIX/reduce -DCMAKE_INSTALL_PREFIX=$HOME/local >> $PREFIX/.messages.txt
-make >> $PREFIX/.messages.txt
-make install >> $PREFIX/.messages.txt
-
-rm -rf $PREFIX/reduce
-rm -rf $PREFIX/build/reduce
-rm $PREFIX/reduce_wwPDB_het_dict.txt
+cmake $PREFIX/reduce -DCMAKE_INSTALL_PREFIX=$HOME/local
+make 
+make install
